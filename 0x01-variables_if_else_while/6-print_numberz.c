e020fa7f6c471c6bcaaff98221876d6894dc0077
Galacticose <mustaphaahmod@gmail.com>
2022-09-09 16:38:47 -0700

    print single digit decimal numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print single digit decimal numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
