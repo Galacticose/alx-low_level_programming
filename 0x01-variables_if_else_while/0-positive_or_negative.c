07d8007ab13d69e5289b70d9c3aaa847f9f7279f
Galacticose <mustaphaahmod@gmail.com>
2022-09-09 05:17:03 -0700

    tell if a number is positive, negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * @n: stores an integer
+ * 
+ * print if n is a positive integer, zero or negative integer
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n>0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if(n==0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else
+	{
+		printf("%i is negative\n", n);
+	}
+	return (0);
+}
