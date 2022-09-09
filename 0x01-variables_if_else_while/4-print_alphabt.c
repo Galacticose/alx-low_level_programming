477b90df0cb59e7f32115eaefc7e067e0e338c21
Galacticose <mustaphaahmod@gmail.com>
2022-09-09 16:05:31 -0700

    print lower case letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,11 +13,11 @@ int main(void)
 
 	for (ch = 'a' ; ch < 'z' ; ch++)
 	{
-		if (ch == 'e' && ch == 'q')
+		if (ch == 'e' || ch == 'q')
 		{
 			continue;
-			putchar(ch);
 		}
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
