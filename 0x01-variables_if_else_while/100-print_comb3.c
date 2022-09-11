21f85bb96a0f34199c98bdbd4bb484e85f22ae7e
Galacticose <mustaphaahmod@gmail.com>
2022-09-11 11:48:42 -0700

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print possible combinations of different digit
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for (b = 0 ; b < 10 ; b++)
+		{
+			if (a == b)
+			{
+				continue;
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
