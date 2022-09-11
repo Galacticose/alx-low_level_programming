236cbb40166dad9e6b1e28264bf48c8c187e32f1
Galacticose <mustaphaahmod@gmail.com>
2022-09-11 13:23:46 -0700

    different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,11 +18,11 @@ int main(void)
 			if (a >= b)
 			{
 				continue;
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(',');
-				putchar(' ');
 			}
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(',');
+			putchar(' ');
 			if (a == 8 && b == 9)
 			{
 				putchar('\n');
