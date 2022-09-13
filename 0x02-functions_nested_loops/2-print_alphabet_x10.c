3bd1c50ae05110a258e7370ba1edb919cb58ea31
Galacticose <mustaphaahmod@gmail.com>
2022-09-13 11:35:46 -0700

    print lowercase alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print lower case letters 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
