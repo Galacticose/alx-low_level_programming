dc29d136c0e5683b97c00552eebb9745f10f9942
Galacticose <mustaphaahmod@gmail.com>
2022-09-13 06:22:01 -0700

    print lower case alphabets with a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * print lowercase alphabets with a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
