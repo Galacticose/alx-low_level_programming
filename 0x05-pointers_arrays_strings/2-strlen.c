dc4bf97a8508753086085b145d22985362a0dcb7
Galacticose <mustaphaahmod@gmail.com>
2022-09-19 09:35:36 -0700

    return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - checks the lenght of a string
+ * @s: stores a character value
+ *
+ * return i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = strlen(s);
+	return (i);
+}
