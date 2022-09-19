d065a377687f375d568936aba850efe974734584
Galacticose <mustaphaahmod@gmail.com>
2022-09-19 10:15:59 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @str: stores a character
+ *
+ * does not return any value
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
