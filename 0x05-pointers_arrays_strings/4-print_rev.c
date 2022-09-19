2bcfee3a189eefc704edc4eedc2e33bd6b4ffb9b
Galacticose <mustaphaahmod@gmail.com>
2022-09-19 15:48:14 -0700

    print a reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,17 +5,14 @@
  * print_rev - prints a string in reverse
  * @s: stores a character
  * @i store an integer value
- * @len: stores an integer value
  *
  * does not return any value
  */
 void print_rev(char *s)
 {
 	int i;
-	int len;
 
-	len = strlen(s);
-	for (i = len - 1 ; i >= 0 ; i--)
+	for (i = strlen(s) - 1 ; i >= 0 ; i--)
 	{
 		_putchar(s[i]);
 	}
