bd167ebc4abcdb1cc5e5ae73e09b6554e6a87d5c
Galacticose <mustaphaahmod@gmail.com>
2022-09-20 05:10:04 -0700

    copy string from an address to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copies a string
+ * @dest: stores a character
+ * @src: stores a character
+ *
+ * Return: always copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
