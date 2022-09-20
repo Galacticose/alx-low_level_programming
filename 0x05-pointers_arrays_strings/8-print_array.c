e10152c7b8563d27b1a14c4b4691b8dbb9ed3227
Galacticose <mustaphaahmod@gmail.com>
2022-09-20 03:54:01 -0700

    print all elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array
+ * @a: stores an integer
+ * @n: stores an integer
+ *
+ * does not return a value
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d ,", a[i]);
+		if (i == n - 1)
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}
