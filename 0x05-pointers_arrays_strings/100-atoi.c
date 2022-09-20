721879518757b6a39fb53c93e3f38ef651739361
Galacticose <mustaphaahmod@gmail.com>
2022-09-20 05:30:28 -0700

    converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - convert string to an integer
+ * @s: stores a character
+ *
+ * Return: Always converted string
+ */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
