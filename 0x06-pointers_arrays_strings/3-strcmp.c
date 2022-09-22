e2e125ec03467978049478400ff26fc87c71b500
Galacticose <mustaphaahmod@gmail.com>
2022-09-22 01:58:32 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: stores a character
+ * @s2: stores a character
+ *
+ * Return: compared strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
