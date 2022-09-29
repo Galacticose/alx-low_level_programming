d8bbb54246ef8dd7bcf2f9a6b017798c42ec4caf
Galacticose <mustaphaahmod@gmail.com>
2022-09-29 00:09:56 -0700

    return the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - get the length of a string
+ * @s: stores a character
+ *
+ * Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
