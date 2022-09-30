a93bd3b3efbae26d6022bd4ba9c73f3be71cc409
Galacticose <mustaphaahmod@gmail.com>
2022-09-30 11:19:46 -0700

    my first static library in c program

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * _puts - prints a string
+ * @s: stores a char
+ *
+ * does not return any value
+ */
 void _puts(char *s)
 {
 	puts(s);
