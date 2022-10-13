0ba50f151b8f39bcdb830b8e0173314560675a78
Galacticose <mustaphaahmod@gmail.com>
2022-10-13 10:28:56 -0700

    print numbers

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all - print different data types
+ * @format: list of types of arguments
+ *
+ * Does not return any value
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char c;
+	int i;
+	float f;
+	char *s;
+	int len;
+}
