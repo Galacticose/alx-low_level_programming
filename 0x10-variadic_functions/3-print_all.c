88a145040597a7aead15e8a42eed92608b213bc3
Galacticose <mustaphaahmod@gmail.com>
2022-10-13 16:16:53 -0700

    print different data types

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <string.h>
 
 /**
  * print_all - print different data types
@@ -11,9 +12,45 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	char c;
 	int i;
+	unsigned int a;
 	float f;
+	char c;
 	char *s;
-	int len;
+	unsigned int len;
+
+	len = strlen(format);
+	a = 0;
+	while (a < len)
+	{
+		va_start(ap, format);
+		switch (format[a])
+		{
+			case 'c':
+				c = va_arg(ap, int);
+				printf("%c, ", c);
+				a++;
+				break;
+			case 'i':
+				i = va_arg(ap, int);
+				printf("%d, ", i);
+				a++;
+				break;
+			case 'f':
+				f = va_arg(ap, double);
+				printf("%f, ", f);
+				a++;
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				printf("%s, ", s);
+				if (s == NULL)
+					printf("(nil)");
+				a++;
+				break;
+		}
+		a++;
+	}
+	printf("\n");
+	va_end(ap);
 }
